1a1944cffaf87c02b6c88327acd7a942cd03fef3
Kyuyeon Kim <pcaso5@naver.com>
2023-01-22 19:56:55 +0900

    [KKY] build error fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,14 +14,14 @@ export default function Home() {
         <div className="border max-w-[220px] max-h-[220px] w-[50vw] h-[50vw] absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 rotate-45 border-murSecondary"></div>
       </section>
       <section className="flex justify-center items-center mt-auto mb-10 gap-2">
-        <a href='https://naver.me/5OGRubmt' className="relative w-[3rem] h-[3rem] overflow-hidden rounded-2xl">
-          <Image src="/static/icons/naver-map-200.png" height={200} width={200} />
+        <a href="https://naver.me/5OGRubmt" className="relative w-[3rem] h-[3rem] overflow-hidden rounded-2xl">
+          <Image src="/static/icons/naver-map-200.png" height={200} width={200} alt="네이버 맵" />
         </a>
-        <a href='https://www.instagram.com/mu_r.shop/' className="relative w-[3rem] h-[3rem] overflow-hidden rounded-2xl">
-          <Image src="/static/icons/instagram-240.png" height={200} width={200} />
+        <a href="https://www.instagram.com/mu_r.shop/" className="relative w-[3rem] h-[3rem] overflow-hidden rounded-2xl">
+          <Image src="/static/icons/instagram-240.png" height={200} width={200} alt="인스타그램" />
         </a>
-        <a href='http://pf.kakao.com/_akbxbxj' className="relative w-[3rem] h-[3rem] overflow-hidden rounded-2xl">
-          <Image src="/static/icons/kakao-240.png" height={200} width={200} />
+        <a href="http://pf.kakao.com/_akbxbxj" className="relative w-[3rem] h-[3rem] overflow-hidden rounded-2xl">
+          <Image src="/static/icons/kakao-240.png" height={200} width={200} alt="카카오 채팅" />
         </a>
       </section>
     </main>
