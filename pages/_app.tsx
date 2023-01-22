e329175f176e887371a658281dd1ba6d55f469f3
Kyuyeon Kim <pcaso5@naver.com>
2023-01-22 20:25:00 +0900

    [KKY] 테일윈드 빌드 테스트 2

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import "../public/static/fonts/style.css";
+import "../build.css"
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
