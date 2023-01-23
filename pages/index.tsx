feea6e77ee0b2a2cf35c70096889ee8e5a56b648
Kyuyeon Kim <pcaso5@naver.com>
2023-01-24 00:33:32 +0900

    [KKY] error fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,13 +15,13 @@ export default function Home() {
       </section>
       <section className="flex justify-center items-center mt-auto mb-10 gap-2">
         <a href='https://naver.me/5OGRubmt' className="relative w-[3rem] h-[3rem] overflow-hidden rounded-2xl">
-          <Image src="/static/icons/naver-map-200.png" height={200} width={200} />
+          <Image src="/static/icons/naver-map-200.png" alt="네이버 지도" height={200} width={200} />
         </a>
         <a href='https://www.instagram.com/mu_r.shop/' className="relative w-[3rem] h-[3rem] overflow-hidden rounded-2xl">
-          <Image src="/static/icons/instagram-240.png" height={200} width={200} />
+          <Image src="/static/icons/instagram-240.png" alt="인스타그램" height={200} width={200} />
         </a>
         <a href='http://pf.kakao.com/_akbxbxj' className="relative w-[3rem] h-[3rem] overflow-hidden rounded-2xl">
-          <Image src="/static/icons/kakao-240.png" height={200} width={200} />
+          <Image src="/static/icons/kakao-240.png" alt="카카오 채팅" height={200} width={200} />
         </a>
       </section>
     </main>
