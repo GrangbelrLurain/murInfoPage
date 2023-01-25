4d9e1557b48c26f28c3722b59117fae2bec7b1e7
lurain <pcaso5@naver.com>
2023-01-25 13:29:38 +0900

    [KKY] import favicon, title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,17 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
-import "../public/static/fonts/style.css";
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
+import '../public/static/fonts/style.css';
+import HomeIcon from '@/public/static/icons/home.png';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>뮤르</title>
+        <link rel='shortcut icon' href={HomeIcon.src} />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
